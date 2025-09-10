76377d15dd35c0b40f096ca973278a5b75c8e86a
Tah Ferreira <tahferreira8@gmail.com>
2025-09-10 16:07:52 -0300

    Atividades

---
+++ Estado.java
@@ -0,0 +1,20 @@
+package Atividades;
+
+public class Estado {
+	
+	private String nome;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	private String sigla;
+
+}

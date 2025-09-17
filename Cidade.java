1503c2d7e466d4c248fe3421e3f10a4851d72aa0
Tah Ferreira <tahferreira8@gmail.com>
2025-09-17 08:56:03 -0300

    Atividades

---
+++ Cidade.java
@@ -0,0 +1,21 @@
+package Atividades;
+
+public class Cidade {
+	
+	private String nome;
+	private Estado estado;
+	
+	protected String getNome() {
+		return nome;
+	}
+	protected void setNome(String nome) {
+		this.nome = nome;
+	}
+	protected Estado getEstado() {
+		return estado;
+	}
+	protected void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+}
---
+++ Estado.java
@@ -0,0 +1,21 @@
+package Atividades;
+
+public class Estado {
+	
+	private String nome;
+	private String sigla;
+	
+	protected String getNome() {
+		return nome;
+	}
+	protected void setNome(String nome) {
+		this.nome = nome;
+	}
+	protected String getSigla() {
+		return sigla;
+	}
+	protected void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	
+}
